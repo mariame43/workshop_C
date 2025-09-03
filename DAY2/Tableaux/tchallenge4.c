a810164e5663d812e69e2ea9c1173e46032bfa28
Mariame <mariamebenslimane69@gmail.com>
2025-09-03 16:24:17 +0100

    tableaux

--- DAY2/Tableaux/tchallenge4.c
+++ DAY2/Tableaux/tchallenge4.c
@@ -1,35 +1,28 @@
 #include <stdio.h>
 
 int main() {
+    int NumberDeElement, max;
 
-
-int NumberDeElement,max;
-    
     printf("Enter number of Items: \n");
-    scanf("%d",&NumberDeElement);
-    
-    int tab[NumberDeElement];
-    for (int i = 0; i < NumberDeElement; i++)
-    {
-           printf("Enter number %d: ",i+1);
-            scanf("%d",&tab[i]);
-    }
-    
-    // search for max
-    max = tab[0]; // WE PUT FIRST NUMBER AS MAX AFTER THAN WE COMPARE IT WITH OTHER NUMBERS
-    
-    for (int i = 1; i < NumberDeElement; i++)
-    {
-        if (tab[i]>max)
-        {
-            max= tab[i];
-        }
-        
-    }
-    
+    scanf("%d", &NumberDeElement);
 
-    printf("le maximan est %d",max);
- 
- 
+    int tab[NumberDeElement];
+
+    for (int i = 0; i < NumberDeElement; i++) {
+        printf("Enter number %d: ", i + 1);
+        scanf("%d", &tab[i]);
+    }
+
+    // search for max
+    max = tab[0]; // First number as max initially
+
+    for (int i = 1; i < NumberDeElement; i++) {
+        if (tab[i] > max) {
+            max = tab[i];
+        }
+    }
+
+    printf("le maximan est %d\n", max);
+
     return 0;
 }
---
+++ DAY2/Tableaux/tchallenge5.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+
+int main() {
+
+
+    int num , mini ;
+
+    printf("entre numero : ");
+    scanf("%d",&num);
+    
+int elle[num];
+    for (int i = 0; i < num; i++) {
+        printf("Enter numero %d: ", i + 1);
+        scanf("%d", &elle[i]);
+    }
+
+    mini = elle[0];
+
+    for (int i = 1; i < num; i++) {
+        if (elle[i] < mini) {
+            mini = elle[i];
+        }
+    }
+
+    printf("le miniman est %d\n", mini);
+
+    return 0;
+}
+
+
+
